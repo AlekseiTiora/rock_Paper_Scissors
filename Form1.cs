6bbf325ddeda49c8a45696699ddc1660d3bad1be
Opilane TTHK <opilane@tthk.ee>
2021-12-02 11:39:52 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rock_Paper_Scissors
+{
+    public partial class Form1 : Form
+    {
+        Button btn;
+        Button btn2;
+        public Form1()
+        {
+            this.Height = 700;//высота
+            this.Width = 900;//ширина
+            this.Text = "Rock_Paper_Scissors";//загловок формы
+            this.BackColor = Color.Wheat;//цвет фона формы
+
+            btn = new Button();
+            btn.BackColor = Color.White;
+            btn.Text = "Two player";
+            btn.Location = new Point(Top = 250, 30);
+            btn.Size = new Size(100, 30);
+            btn.Click += Btn_Click;
+
+            btn2 = new Button();
+            btn2.BackColor = Color.White;
+            btn2.Text = "Play with bot";
+            btn2.Location = new Point(Top = 490, 30);
+            btn2.Size = new Size(100, 30);
+            btn2.Click += Btn2_Click;
+
+
+            this.Controls.Add(btn2);
+            this.Controls.Add(btn);
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rock_Paper_Scissors
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            this.Height = 700;//высота
+            this.Width = 900;//ширина
+            this.Text = "Rock_Paper_Scissors";//загловок формы
+            this.BackColor = Color.Wheat;//цвет фона формы
+        }
+    }
+}
